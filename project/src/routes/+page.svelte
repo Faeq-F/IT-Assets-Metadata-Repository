d33a507911d6925988f1b590139bbb9ba3336714
Faeq <faeqfaisal@hotmail.co.uk>
2024-01-26 09:23:15 +0000

    Setup login navigation for this sprint

--- project/src/routes/+page.svelte
+++ project/src/routes/+page.svelte
@@ -1,14 +1,50 @@
 <script>
-	/**
-	 * This is example JsDoc.
-	 * @param {number} n - The title of the book.
-	 * @param {number} m - The author of the book.
-	 */
-	function modulo(n, m) {
-		return ((n % m) + m) % m;
-	}
 </script>
 
-<h1>Team Project</h1>
-<p>CS2815</p>
-<p>modulo(7, 3) is {modulo(7, 3)}</p>
+<svelte:head>
+	<title>Login</title>
+</svelte:head>
+
+<form action="" class="userForm">
+	<label for="username" class="formLabel">Username:</label>
+	<input type="text" id="username" name="username" /><br /><br />
+	<label for="password" class="formLabel">Password:</label>
+	<input type="text" id="password" name="password" /><br /><br />
+	<a href="/home">
+		<button class="gradientButton submitButton">
+			<span class="text">Log in</span>
+		</button>
+	</a>
+</form>
+
+<style>
+	@import url('$lib/styles/root.css');
+
+	.userForm {
+		margin: 0;
+		padding: 0;
+		/** Center the form */
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.submitButton {
+		margin: 0 auto;
+		font-size: 1.3em;
+		width: 100%;
+	}
+
+	.formLabel {
+		font-size: 1.3em;
+	}
+
+	/**
+		reuse button style from faeqs site
+		maybe make gradient buttons a seperate component and then nest it in this page
+		@import url('$lib/styles/gradientButtons.css');
+	*/
+	.gradientButton {
+	}
+</style>
