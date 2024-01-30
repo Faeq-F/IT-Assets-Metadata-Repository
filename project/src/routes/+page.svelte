bd2d9e79bc8ad32573ffe8c2547e99bfcd708027
Faeq <faeqfaisal@hotmail.co.uk>
2024-01-30 14:08:45 +0000

    Merged database project with initial setup - all database requests now work

--- project/src/routes/+page.svelte
+++ project/src/routes/+page.svelte
@@ -1,14 +1,53 @@
 <script>
-	/**
-	 * This is example JsDoc.
-	 * @param {number} n - The title of the book.
-	 * @param {number} m - The author of the book.
-	 */
-	function modulo(n, m) {
-		return ((n % m) + m) % m;
+	let name = 'Asset App';
+	let API_URL = 'http://localhost:5038/';
+	let assets = [];
+	let newAssets = '';
+	function refreshAssets() {
+		fetch(API_URL + 'api/teamproject/GetAssets')
+			.then((response) => response.json())
+			.then((data) => {
+				assets = data;
+			});
+	}
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		refreshAssets();
+	});
+	function addAssets() {
+		console.log('in');
+		const data = new FormData();
+		data.append('newAssets', newAssets);
+		fetch(API_URL + 'api/teamproject/AddAssets', {
+			method: 'POST',
+			body: data
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				alert(data);
+				refreshAssets();
+			});
+	}
+	function deleteAssets(id) {
+		fetch(API_URL + 'api/teamproject/DeleteAssets?id=' + id, {
+			method: 'DELETE'
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				alert(data);
+				refreshAssets();
+			});
 	}
 </script>
 
-<h1>Team Project</h1>
-<p>CS2815</p>
-<p>modulo(7, 3) is {modulo(7, 3)}</p>
+<main>
+	<h2>{name}</h2>
+	<input bind:value={newAssets} />
+	<button on:click={addAssets}>Add Assets</button>
+	{#each assets as asset}
+		<p>
+			<b>*{asset.asset_name}</b>
+			<button on:click={deleteAssets(asset.id)}>Delete Assets</button>
+		</p>
+	{/each}
+</main>
---
+++ project/src/routes/api/index.cjs
@@ -0,0 +1,42 @@
+var Express = require('express');
+var Mongoclient = require('mongodb').MongoClient;
+var cors = require('cors');
+const multer = require('multer');
+const { request, response } = require('express');
+
+var app = Express();
+app.use(cors());
+
+var URI = 'mongodb+srv://jack:1234@cluster0.lwomw.mongodb.net/?retryWrites=true&w=majority';
+var DATABASEName = 'Teamproject';
+var database;
+
+app.listen(5038, () => {
+	Mongoclient.connect(URI, (error, client) => {
+		database = client.db(DATABASEName);
+		console.log('Connected');
+	});
+});
+app.get('/api/teamproject/GetAssets', (request, reponse) => {
+	database
+		.collection('Teamproject')
+		.find({})
+		.toArray((error, result) => {
+			reponse.send(result);
+		});
+});
+app.post('/api/teamproject/AddAssets', multer().none(), (request, response) => {
+	database.collection('Teamproject').count({}, function (error, numOfAssets) {
+		database.collection('Teamproject').insertOne({
+			id: (numOfAssets + 1).toString(),
+			asset_name: request.body.newAssets
+		});
+		response.json('Added sir');
+	});
+});
+app.delete('/api/teamproject/DeleteAssets', (request, response) => {
+	database.collection('Teamproject').deleteOne({
+		id: request.query.id
+	});
+	response.json('Deleted');
+});
