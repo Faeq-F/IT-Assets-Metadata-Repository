9fdc34b556c7417ccc0ef59695718634b130335b
zlac334 <zlac334@live.rhul.ac.uk>
2024-02-09 13:40:06 +0000

    Added a listbox which will allow the user to select what filters they want to apply (using placeholder filters). Also added a apply filters button which is nonfunctional currently

---
+++ project/src/routes/assets/filterDrawer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	let valueMultiple: string[] = [];
+</script>
+<h1 class="h1">
+    Filters
+</h1>
+<div class="card" id="filters">
+	<ListBox multiple>
+		<ListBoxItem bind:group={valueMultiple} name="medium" value="type1">type1</ListBoxItem>
+		<ListBoxItem bind:group={valueMultiple} name="medium" value="type2">type2</ListBoxItem>
+		<ListBoxItem bind:group={valueMultiple} name="medium" value="type3">type3</ListBoxItem>
+	</ListBox>
+
+</div>
+<div class="card" id="applyButton">    
+    <button type="button"> Apply Filters</button>
+</div>
+
+<style>     
+    #filters{
+        margin-left: 25%;
+        margin-right: 25%;
+    }
+    #applyButton{
+        text-align: center;
+        margin-top: 5%;
+        margin-left: 45%;
+        margin-right: 45%;
+    }
+    .h1 {
+		text-align: center;
+		margin-top: 15%;
+        margin-bottom: 5%;
+	}
+</style>
