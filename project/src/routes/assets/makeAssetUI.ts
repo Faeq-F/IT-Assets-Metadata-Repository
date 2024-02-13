90ba4a558dafbf1e3670364910e349ab8681bfc9
Faeq <faeqfaisal@hotmail.co.uk>
2024-02-13 14:26:57 +0000

    made make-asset dialog save to db, preserving data types

--- project/src/routes/assets/makeAssetUI.ts
+++ project/src/routes/assets/makeAssetUI.ts
@@ -4,6 +4,9 @@ import { onMount } from 'svelte';
 
 import { fetchDocuments } from '../api/apiRequests';
 
+/**
+ * Dynamically adds the saved types for assets into the dropdown in the makeAsset ui
+ */
 export function addTypeOptions() {
 	onMount(() => {
 		if (browser) {
@@ -48,12 +51,7 @@ export function addTypeOptions() {
 
 								child = document.createElement('someuniquetag');
 
-								child.innerHTML =
-									'<input type="text" class="input w-96" id="' +
-									i.field +
-									'" name="' +
-									i.field +
-									'" /><br /><br />';
+								child.innerHTML = generateInputForType(i);
 
 								if (metadataForm) metadataForm.appendChild(child);
 
@@ -64,4 +62,61 @@ export function addTypeOptions() {
 			});
 		}
 	});
+}
+
+/**
+ * Creates the relevant input for a metadata field based on its data type
+ * @param fieldObject the metadata field to create an input for
+ * @returns the innerHTML for a child node in the metadataForm, resembling the input(s) for the metadata field
+ */
+function generateInputForType(fieldObject: any):string {
+	let input = ""
+	if (fieldObject.list) {
+		switch (fieldObject.dataType) {
+			case "Text":
+				input = '<p></p><input type="text" placeholder="please ensure there is a \', \' (Comma + Space) between the text values" class="input w-11/12" id="' +
+					fieldObject.field +
+					'-Text-ListIDaddon" name="' +
+					fieldObject.field +
+					'" /><br /><br />';
+				break;
+			case "Number":
+				input = '<input type="text" placeholder="please ensure there is a \', \' (Comma + Space) between the number values" pattern="\d*" class="input w-11/12" id="' +
+					fieldObject.field +
+					'-Number-ListIDaddon" name="' +
+					fieldObject.field +
+					'" /><br /><br />';
+				break;
+			case "Asset":
+				input = 'The application cannot associate assets yet - coming soon!<br><br>';
+				break;
+			case "Account":
+				input = 'The application cannot associate accounts yet - coming soon!<br><br>';
+				break;
+		}
+	} else {
+		switch (fieldObject.dataType) {
+			case "Text":
+				input = '<input type="text" class="input w-96" id="' +
+					fieldObject.field +
+					'" name="' +
+					fieldObject.field +
+					'" /><br /><br />';
+				break;
+			case "Number":
+				input = '<input type="number" class="input w-96" id="' +
+					fieldObject.field +
+					'" name="' +
+					fieldObject.field +
+					'" /><br /><br />';
+				break;
+			case "Asset":
+				input = 'The application cannot associate assets yet - coming soon!<br><br>';
+				break;
+			case "Account":
+				input = 'The application cannot associate accounts yet - coming soon!<br><br>';
+				break;
+		}
+	}
+	return input;
 }
