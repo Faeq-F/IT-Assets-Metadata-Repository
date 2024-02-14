bc75ca29e8263d478b78d8b479cf3b92b538e432
Faeq <faeqfaisal@hotmail.co.uk>
2024-02-14 19:57:08 +0000

    Styled Asset type cards & added badges for data types & listables

--- project/src/routes/types/typeDivInjection.ts
+++ project/src/routes/types/typeDivInjection.ts
@@ -6,16 +6,24 @@ export async function injectTypeDivs():Promise<boolean> {
 		typesContainer.innerHTML = "";
 		if (assetTypeDocuments.length > 0) {
 			for (let i of assetTypeDocuments) {
-				var child = document.createElement('someuniquetag');
-				child.innerHTML =
-					'<div class="card assetCard variant-ghost-surface" style="padding: 10px; margin: 10px;">' +
+				let temp = '<div style="padding:10px;border-radius:5px;background-color: #ffffff; box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,rgba(0, 0, 0, 0.3) 0px 1px 3px -1px; margin: 10px;" style="padding: 20px; margin: 10px;"><div class="h3" style="margin:10px; font-weight: bold;">' +
 					i.typeName +
-					'<br /><br />Fields required: ' +
-					JSON.stringify(i.metadataFields) +
-					'</div>';
+					'</div><div style="margin: 10px; font-weight: 500">Fields required:</div><ul>';
 
+				for (let j of i.metadataFields) {
+					temp += "<li style='margin-top:5px;'>" + j.field+`<span class='card assetCard variant-ghost-surface' style='margin-left: 5px;padding: 2px 5px;'>${j.dataType}</span>`;
+
+					if (j.list) {
+						temp+="<span class='card assetCard variant-ghost-surface' style='margin-left: 5px;padding: 2px 5px;'>Multi-value (Listable)</span>"
+					}
+
+					temp+="</li>"
+				}
+				temp += "</ul></div>"
+
+				var child = document.createElement('someuniquetag');
+				child.innerHTML = temp;
 				typesContainer.appendChild(child);
-
 				child.outerHTML = child.outerHTML.replace(/<\/?someuniquetag>/, '');
 			}
 			return true;
@@ -24,3 +32,8 @@ export async function injectTypeDivs():Promise<boolean> {
 	});
 
 }
+
+
+
+//card assetCard variant-ghost-surface
+//padding:5px;border-radius:5px;background-color: #ffffff; box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
