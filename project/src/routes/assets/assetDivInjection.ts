1aa20ced854aa09cf41e31feb962bdb3808fa486
Faeq <faeqfaisal@hotmail.co.uk>
2024-02-19 13:12:57 +0000

    Added card bg to Assets & placed stringified JSON in custom element data attribute

--- project/src/routes/assets/assetDivInjection.ts
+++ project/src/routes/assets/assetDivInjection.ts
@@ -8,22 +8,30 @@ export function injectAssetDivs(): Promise<boolean> {
 			for (let i of assetDocuments) {
 				var child = document.createElement('someuniquetag');
 				child.innerHTML =
-					'<div class="card assetCard variant-ghost-surface" style="padding: 10px; margin: 10px;"><pre>' +
+					'<div style="z-index:1;position: relative;padding:10px;border-radius:5px;background-color: #ffffff; box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,rgba(0, 0, 0, 0.3) 0px 1px 3px -1px; margin: 10px;" style="padding: 20px; margin: 10px;" data-stringifiedjson=\''+ JSON.stringify(i) +'\' class="card assetCard variant-ghost-surface" style="padding: 10px; margin: 10px;">' +
 					i.assetName +
-					'<br /><br />Link: ' +
+					'Link: ' +
 					i.assetLink +
-					'<br /><br />Type: ' +
+					'Type: ' +
 					i.assetType +
-					'<br /><br />Metadata: ' +
+					'Metadata: ' +
 					JSON.stringify(i.metadataFields, null, '\n') +
-					'</pre></div>';
+					'</div>';
 
 				assetsContainer.appendChild(child);
 
 				child.outerHTML = child.outerHTML.replace(/<\/?someuniquetag>/, '');
 			}
+			injectDivStyles();
 			return true;
 		}
 		return false;
 	});
+}
+
+export function injectDivStyles() {
+	const assetsContainer = document.getElementsByClassName('assetsContainer')[0];
+	for (let i of assetsContainer.getElementsByTagName('DIV')) {
+		console.log(i);
+	}
 }
