c31597955515537ff4d0d2a9b825db607df12409
Faeq <faeqfaisal@hotmail.co.uk>
2024-02-25 18:55:51 +0000

    removed highlight.js since it is no longer needed

--- project/src/routes/+layout.svelte
+++ project/src/routes/+layout.svelte
@@ -4,21 +4,6 @@
 	import { page } from '$app/stores'; //Does work
 	import { AppShell, Modal } from '@skeletonlabs/skeleton';
 
-	// Highlight JS
-	import hljs from 'highlight.js/lib/core';
-	import 'highlight.js/styles/github-dark.css';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
-	import xml from 'highlight.js/lib/languages/xml'; // for HTML
-	import css from 'highlight.js/lib/languages/css';
-	import javascript from 'highlight.js/lib/languages/javascript';
-	import typescript from 'highlight.js/lib/languages/typescript';
-
-	hljs.registerLanguage('xml', xml); // for HTML
-	hljs.registerLanguage('css', css);
-	hljs.registerLanguage('javascript', javascript);
-	hljs.registerLanguage('typescript', typescript);
-	storeHighlightJs.set(hljs);
-
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
