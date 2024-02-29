f6d50839d9b4abe383a64d3fd765c3bf12deed8e
Thomas <Thomas.Appleby@rhul.ac.uk>
2024-02-29 13:01:28 +0000

    Added page for managing permissions

---
+++ project/src/routes/permissions/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    //@ts-ignore
+    import { browser } from '$app/environment'; //Does work
+	import { onMount } from 'svelte';
+	import { fetchDocuments } from '../api/apiRequests';
+    import {
+		AppBar,
+		getModalStore,
+		InputChip,
+		type ModalComponent,
+		type ModalSettings
+	} from '@skeletonlabs/skeleton';
+	import Cookies from 'js-cookie';
+
+    onMount(async() => {
+        fetchDocuments('User').then((Users) => {
+            CurrentUsers = Users;
+        });
+    });
+
+    let CurrentUsers: any[];
+</script>
+
+<svelte:head>
+    <title>Permissions</title>
+</svelte:head>
+
+<h1 class='h1'>Manage Permissions</h1>
