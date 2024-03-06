052e5506cdca670a8d7c345d1d4ba9cc3a24d171
Alex <alex.aleksandar.2022@live.rhul.ac.uk>
2024-03-06 20:44:07 +0000

    Modified 'Update username & password' test and added a new test 'Update only password'

--- project/tests/integration/UpdateAccount.test.js
+++ project/tests/integration/UpdateAccount.test.js
@@ -21,7 +21,7 @@ test.beforeEach('Login as the test account', async ({ page }) => {
 	await page.goto('/profile');
 });
 
-test('Update username & password', async ({ page }) => {
+test('Update username & password, attempt to sign in (with old/new details)', async ({ page }) => {
 	// Check username is as expected
 	await expect(page.getByTitle('Username')).toHaveText('Username: ' + username);
 
@@ -36,6 +36,49 @@ test('Update username & password', async ({ page }) => {
 
 	// Check if the username has been updated
 	await expect(page.getByTitle('Username')).toHaveText('Username: ' + newUsername);
+
+	// Logout and return login
+	await page.getByRole('button', { name: 'Logout' }).click();
+	await delay(time);
+	expect(page.url()).toBe(site);
+
+	//Attempt to sign in with old details
+	await page.getByLabel('Username').fill(username);
+	await page.getByLabel('Password').fill(password);
+	await page.getByRole('button', { name: 'Log in' }).click();
+	await delay(time);
+
+	let url = await page.url();
+	if (url == site + 'home') {
+		// User should not be able to login with old details
+		test.fail();
+	}
+
+	// Sign in with the updated details
+	await page.getByLabel('Username').fill(newUsername);
+	await page.getByLabel('Password').fill(newPassword);
+	await page.getByRole('button', { name: 'Log in' }).click();
+	await delay(time);
+
+	expect(page.url()).toBe(site + 'home');
+	await page.goto('/profile');
+});
+
+test('Update only password', async ({ page }) => {
+	// Check username is as expected
+	await expect(page.getByTitle('Username')).toHaveText('Username: ' + username);
+
+	await page.getByRole('button', { name: 'Update Account details' }).click();
+	await page.getByLabel('username').fill(username);
+	await page.getByPlaceholder('Enter new Password...').fill(newPassword);
+	await page.getByPlaceholder('Enter Password again...').fill(newPassword);
+	await page.getByRole('button', { name: 'Update details' }).click();
+
+	// Wait (time) seconds for the update to register
+	await delay(time);
+
+	// Check if the username remains
+	await expect(page.getByTitle('Username')).toHaveText('Username: ' + username);
 });
 
 test.afterEach('Change details back', async ({ page }) => {
