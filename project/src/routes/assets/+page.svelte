dad8f1ac437a27018c1717e013a548b51803dd42
Faeq <faeqfaisal@hotmail.co.uk>
2024-03-10 08:51:55 +0000

    Made keyword search work on the displayed values of associations & used placeholder asset element for loading

--- project/src/routes/assets/+page.svelte
+++ project/src/routes/assets/+page.svelte
@@ -154,16 +154,39 @@
 <div class="assetsContainer">
 	{#if AssetDocuments != undefined}
 		{#each AssetDocuments as asset}
-			{#if keywordFilter(asset, keywordSearchInput) && (filters.includes(asset.assetType) || filters.length == 0)}
-				<Asset
-					id={asset._id}
-					name={highlight(asset.assetName, keywordSearchInput)}
-					link={highlight(asset.assetLink, keywordSearchInput)}
-					type={highlight(asset.assetType, keywordSearchInput)}
-					metadata={asset.metadataFields}
-					{keywordSearchInput}
-				/>
-			{/if}
+			{#await keywordFilter(asset, keywordSearchInput)}
+				<div class="card card-hover bg-modern-50 m-10 w-6/12 drop-shadow-md">
+					<div class="card-header flex items-center justify-between">
+						<div class="flex animate-pulse items-center justify-center space-x-4">
+							<div class="placeholder-circle w-16" />
+							<div class="placeholder-circle w-14" />
+							<div class="placeholder-circle w-10" />
+						</div>
+					</div>
+					<div class="animate-pulse space-y-4 p-4">
+						<div class="placeholder" />
+						<div class="grid grid-cols-4 gap-4">
+							<div class="placeholder" />
+							<div class="placeholder" />
+							<div class="placeholder" />
+							<div class="placeholder" />
+						</div>
+						<div class="placeholder" />
+						<div class="placeholder" />
+					</div>
+				</div>
+			{:then shouldShow}
+				{#if shouldShow && (filters.includes(asset.assetType) || filters.length == 0)}
+					<Asset
+						id={asset._id}
+						name={highlight(asset.assetName, keywordSearchInput)}
+						link={highlight(asset.assetLink, keywordSearchInput)}
+						type={highlight(asset.assetType, keywordSearchInput)}
+						metadata={asset.metadataFields}
+						{keywordSearchInput}
+					/>
+				{/if}
+			{/await}
 		{/each}
 	{/if}
 </div>
