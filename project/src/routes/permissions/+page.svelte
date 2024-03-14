4d09793148b3aa5bba4171ab3fac9d739380e05f
Faeq <faeqfaisal@hotmail.co.uk>
2024-03-14 17:30:10 +0000

    Fixed cookies on update roles & added icon for back button

--- project/src/routes/permissions/+page.svelte
+++ project/src/routes/permissions/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	//@ts-ignore
 	import { browser } from '$app/environment';
-
 	import { onMount } from 'svelte';
 	import { fetchDocuments, updateDocument } from '../api/apiRequests';
 	import { redirectWhenNotLoggedIn } from '$lib/scripts/loginSaved';
 	import Cookies from 'js-cookie';
+	import { getToastStore } from '@skeletonlabs/skeleton';
+	const toastStore = getToastStore();
 
 	// Define an interface for user data structure
 	interface User {
@@ -36,54 +37,61 @@
 		}
 	});
 
-
 	function updateUserPermissions() {
 		let selects = document.getElementsByTagName('SELECT');
 		for (let i of selects) {
 			fetchDocuments('User').then((userDocument) => {
-				let user = userDocument.find((user: {username: string}) => user.username === (i as HTMLSelectElement).dataset.username);
+				let user = userDocument.find(
+					(user: { username: string }) =>
+						user.username === (i as HTMLSelectElement).dataset.username
+				);
 				let password = user.passwordHash;
 				let newUserObject = {
 					username: (i as HTMLSelectElement).dataset.username,
 					email: (i as HTMLSelectElement).dataset.email,
 					role: (i as HTMLSelectElement).value,
-					passwordHash: password,
+					passwordHash: password
 				};
-				console.log(newUserObject);
-		
+				if (newUserObject.username == Cookies.get('savedLogin-username')) {
+					Cookies.set('savedLogin-role', '' + newUserObject.role, { expires: 70 });
+					Cookies.set('savedLogin-username', '' + newUserObject.username, { expires: 70 });
+					Cookies.set('savedLogin-email', '' + newUserObject.email, { expires: 70 });
+					Cookies.set('savedLogin-password', '' + newUserObject.passwordHash, { expires: 70 });
+				}
 				for (let i of userDocument) {
 					if (i.username == newUserObject.username) {
 						const data = new FormData();
 						data.append('newData', JSON.stringify(newUserObject));
-
 						updateDocument('User', i._id, data)
 							.then((response) => {
 								console.log(response);
+								toastStore.trigger({
+									message: 'Roles updated',
+									background: 'variant-ghost-success',
+									timeout: 3000
+								});
 							})
 							.catch((error) => {
-								console.error('Update user role error', error)
-							})
-						Cookies.set('savedLogin-role', '' + i.role, { expires:70 });
-						Cookies.set('savedLogin-username', '' + newUserObject.username, { expires: 70});
-						Cookies.set('savedLogin-email', '' + newUserObject.email, { expires: 70});
-						Cookies.set('savedLogin-password', '' + newUserObject.passwordHash, { expires: 70});
+								console.error('Update user role error', error);
+							});
 					}
-				}	
+				}
 			});
 		}
 	}
-
 </script>
 
 <svelte:head>
 	<title>Permissions</title>
 </svelte:head>
-<a class="variant-filled-primary btn" href="/profile" rel="nonreffere"> Back </a>
+<a class="variant-filled-surface btn ml-4" href="/profile" rel="nonreffere"
+	><i class="fa-solid fa-circle-arrow-left"></i>&nbsp;&nbsp;Back
+</a>
 
 <h1 class="h1">Manage Permissions</h1>
 <div class="usersContainer" id="usersContainer">
 	{#each users as user}
-		<div class="card card-hover m-4 bg-modern-50 p-3.5 drop-shadow-md">
+		<div class="card card-hover bg-modern-50 m-4 p-3.5 drop-shadow-md">
 			<div class="h3 font-bold">{user.username}</div>
 			<div class="h4">{user.email}</div>
 			<br />
@@ -103,8 +111,8 @@
 	{/each}
 </div>
 <button id="updatePermissions" class="variant-filled-primary btn" on:click={updateUserPermissions}
-	>Update user permissions</button>
-
+	>Update user permissions</button
+>
 
 <style>
 	#updatePermissions {
