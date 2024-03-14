344dbfa5c61610a0f63f58288347b865dac2a30b
ZLAC334 <ZLAC334@live.rhul.ac.uk>
2024-03-14 10:37:03 +0000

    Added the add and remove components for the audit trail

---
+++ project/src/lib/components/cards/auditTrail/Remove.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let key: string;
+	export let newValue: any;
+</script>
+
+<div class="card variant-ghost-error m-2 p-4">
+	<div><span class="font-bold">Updated: </span>{key}</div>
+	<div><span class="font-bold">To: </span>{newValue}</div>
+</div>
