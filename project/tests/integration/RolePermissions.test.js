ff6c6f311f0232e04c225e9c6af38c38a8096323
Alex <alex.aleksandar.2022@live.rhul.ac.uk>
2024-03-19 15:41:30 +0000

    Finished tests for an admin user and added tests for normal users and viewers

--- project/tests/integration/RolePermissions.test.js
+++ project/tests/integration/RolePermissions.test.js
@@ -3,10 +3,19 @@ const delay = (/** @type {number} */ ms) => new Promise((resolve) => setTimeout(
 const time = 500;
 const site = 'http://localhost:4173/';
 
+const adminUser = 'root';
+const adminPass = 'rootroot12';
+
+const normalUser = 'normal';
+const normalPass = 'password123';
+
+const viewerUser = 'viewer';
+const viewerPass = 'password123';
+
 test('Test permissions of an admin user ', async ({ page }) => {
 	await page.goto('/');
-	await page.getByLabel('Username').fill('root');
-	await page.getByLabel('Password').fill('rootroot12');
+	await page.getByLabel('Username').fill(adminUser);
+	await page.getByLabel('Password').fill(adminPass);
 	await page.getByRole('button', { name: 'Log in' }).click();
 	// Wait (time) seconds for the login to process
 	await delay(time);
@@ -14,10 +23,83 @@ test('Test permissions of an admin user ', async ({ page }) => {
 
 	// Navigate to profile and expect to see the "Manage user permissions" button
 	await page.goto('/profile');
-    await expect(page.getByTitle('Role')).toHaveText('Role: admin');
+	await expect(page.getByTitle('Role')).toHaveText('Role: admin');
 	await expect(page.getByText('Manage user permissions')).toBeVisible();
-    
-    // Expect the admin user can navigate to the permissions page
-    await page.goto('/permissions');
-    await expect(page.getByRole('button', { name: 'Update user permissions' })).toBeVisible();
+
+	// Expect the admin user can navigate to the permissions page
+	await page.goto('/permissions');
+	await expect(page.getByRole('button', { name: 'Update user permissions' })).toBeVisible();
+
+	await page.goto('/types');
+	// Expect button (to create asset type) to be visible
+	await expect(page.locator('#assetMaker')).toBeVisible();
+
+	await page.goto('/assets');
+	await expect(page.locator('#assetMaker')).toBeVisible();
+});
+
+test('Test permissions of a normal user ', async ({ page }) => {
+	await page.goto('/');
+	await page.getByLabel('Username').fill(normalUser);
+	await page.getByLabel('Password').fill(normalPass);
+	await page.getByRole('button', { name: 'Log in' }).click();
+	// Wait (time) seconds for the login to process
+	await delay(time);
+	expect(page.url()).toBe(site + 'home');
+
+	// Navigate to profile and expect to see the "Manage user permissions" button
+	await page.goto('/profile');
+	await expect(page.getByTitle('Role')).toHaveText('Role: normal');
+	await expect(page.getByText('Manage user permissions')).toBeHidden();
+
+	// Expect a normal user to be redirected when attempting to access permissions page
+	await page.goto('/permissions');
+	await delay(time);
+	expect(page.url()).toBe(site + 'profile');
+
+	let url = await page.url();
+	if (url == site + 'permissions') {
+		// Normal user shouldn't be able to access the permissions page
+		test.fail();
+	}
+
+	await page.goto('/types');
+	// Expect button (to create asset type) to be invisible
+	await expect(page.locator('#assetMaker')).toBeHidden();
+
+	await page.goto('/assets');
+	await expect(page.locator('#assetMaker')).toBeVisible();
+});
+
+test('Test permissions of a viewer user ', async ({ page }) => {
+	await page.goto('/');
+	await page.getByLabel('Username').fill(viewerUser);
+	await page.getByLabel('Password').fill(viewerPass);
+	await page.getByRole('button', { name: 'Log in' }).click();
+	// Wait (time) seconds for the login to process
+	await delay(time);
+	expect(page.url()).toBe(site + 'home');
+
+	// Navigate to profile and expect to see the "Manage user permissions" button
+	await page.goto('/profile');
+	await expect(page.getByTitle('Role')).toHaveText('Role: viewer');
+	await expect(page.getByText('Manage user permissions')).toBeHidden();
+
+	// Expect a viewer user to be redirected when attempting to access permissions page
+	await page.goto('/permissions');
+	await delay(time);
+	expect(page.url()).toBe(site + 'profile');
+
+	let url = await page.url();
+	if (url == site + 'permissions') {
+		// Normal user shouldn't be able to access the permissions page
+		test.fail();
+	}
+
+	await page.goto('/types');
+	// Expect button (to create asset type) to be invisible
+	await expect(page.locator('#assetMaker')).toBeHidden();
+
+	await page.goto('/assets');
+	await expect(page.locator('#assetMaker')).toBeHidden();
 });
--- project/tests/unit/dbDependant/deleteUser.test.js
+++ project/tests/unit/dbDependant/deleteUser.test.js
@@ -4,38 +4,34 @@ import { hashCode } from '$lib/scripts/validate';
 
 const password = 'password123';
 const passwordHash = hashCode(password);
-const username = "testDeletingUser";
-const email = "testDelete@mail.com";
+const username = 'testDeletingUser';
+const email = 'testDelete@mail.com';
 const role = 'viewer';
-const userObj = { username: username, passwordHash: passwordHash, email: email, role: role};
+const userObj = { username: username, passwordHash: passwordHash, email: email, role: role };
 
 test('Delete user account', async () => {
-    // Create a new user and insert it into the database
-    const data = new FormData();
-    data.append('newData', JSON.stringify(userObj));
-    insertDocument('User', data);
+	// Create a new user and insert it into the database
+	const data = new FormData();
+	data.append('newData', JSON.stringify(userObj));
+	insertDocument('User', data);
 
-    fetchDocuments('User').then((documentsReturned) => {
-        for (let i of documentsReturned) {
-            if (i.username == username) {
-                
-                //User was found, so delete it from the table
-                deleteDocument('User', i._id);
+	fetchDocuments('User').then((documentsReturned) => {
+		for (let i of documentsReturned) {
+			if (i.username == username) {
+				//User was found, so delete it from the table
+				deleteDocument('User', i._id);
 
-                // Check if the user appears in the table again
-                fetchDocuments('User').then((documentsReturned) => {
-                    for (let i of documentsReturned) {
-                        if (i.username == username) {
-                            assert.fail('User should not exist in the table after deletion')
-                        }
-                    }
-                 })
-            } 
-        }
+				// Check if the user appears in the table again
+				fetchDocuments('User').then((documentsReturned) => {
+					for (let i of documentsReturned) {
+						if (i.username == username) {
+							assert.fail('User should not exist in the table after deletion');
+						}
+					}
+				});
+			}
+		}
 
-        assert.fail('Inserted user not found')
-
-    })
-
-    
+		assert.fail('Inserted user not found');
+	});
 });
