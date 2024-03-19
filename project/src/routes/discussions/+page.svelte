4d254d8326b7c6367f301a238ca27bc8c83e06ca
Faeq <faeqfaisal@hotmail.co.uk>
2024-03-19 14:45:09 +0000

    Added discussions page

---
+++ project/src/routes/discussions/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	//@ts-ignore
+	import { browser } from '$app/environment'; //Does work
+	import { onMount } from 'svelte';
+	import { redirectWhenNotLoggedIn } from '$lib/scripts/loginSaved';
+	import { fetchDocuments } from '$lib/apiRequests';
+
+	onMount(() => {
+		if (browser) {
+			redirectWhenNotLoggedIn();
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>Discussions</title>
+</svelte:head>
+
+<h1 class="h1">Discussions</h1>
+<br />
+<div class="card bg-modern-50 w-9/12 object-center shadow-md" id="HomeCard">
+	<p>
+		Discussion boards can be about anything. Certain projects that have been undertaken, or general
+		questions. You can organize information being talked about in containers.
+	</p>
+</div>
+
+<div class="card bg-modern-50 w-6/6 m-4 mx-48 p-5 shadow-md"></div>
+
+<style>
+	#HomeCard {
+		padding: 20px;
+		margin: 0 auto;
+		text-align: center;
+	}
+</style>
