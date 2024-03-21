74992453687d0197b05cfc4a183bb9138af0f106
Shurjeel <zltm723@live.rhul.ac.uk>
2024-03-21 21:16:00 +0000

    Creating test to updateAssetType

--- project/tests/unit/dbDependant/update.test.js
+++ project/tests/unit/dbDependant/update.test.js
@@ -1,13 +1,43 @@
 import { expect, test } from 'vitest';
-//import { updateDocument } from '$lib/apiRequests';
+import { updateDocument, insertDocument, fetchDocuments } from '$lib/apiRequests';
+//insert assetType to test
+function insertType() {
+	var assetType = {
+		typeName: 'testType',
+		metadataFields: 'metadataObject'
+	};
 
-// test('UpdateAssetType', async () => {
-// 	const data = new FormData();
-// 	data.append('newData', JSON.stringify({ typeName: 'g', metadataFields: ['h', 'g'] }));
-// 	return updateDocument('AssetType', '65d4aa1ce91cc1e107af75e1', data).then((response) => {
-// 		console.log(response);
-// 	});
-// });
+	const data = new FormData();
+	data.append('newData', JSON.stringify(assetType));
+
+	insertDocument('AssetType', data).then((response) => {
+		console.log(response);
+	});
+}
+
 test('adds 2 + 2 to equal 4', () => {
 	expect(2 + 2).toBe(4);
+});
+
+//insertType();
+
+test('UpdateAssetType', () => {
+	var assetType = {
+		typeName: 'newTestType',
+		metadataFields: 'newMetadataObject'
+	};
+
+
+	const data = new FormData();
+	data.append('newData', JSON.stringify(assetType));
+
+	updateDocument('AssetType', '65fca2825da68c6811c53359', data).then(async (response) => {
+		console.log(response);
+	});
+
+
+	return fetchDocuments('AssetType').then(async (answer) => {
+		console.log(answer[7].typeName);
+		return expect(answer[7].typeName).toEqual('newTestType');
+	}); 
 });
