206d3a753ef0c46dc137b0bec461cab1ca7ff59d
Christian Cubos <zlac145@live.rhul.ac.uk>
2024-03-21 14:33:12 +0000

    Added extra security for redirecting ensuring that cookies contain a valid user credentials

--- project/src/lib/scripts/loginSaved.ts
+++ project/src/lib/scripts/loginSaved.ts
@@ -1,20 +1,62 @@
 import Cookies from 'js-cookie';
-export function redirectWhenLoginSaved() {
+import { fetchDocuments } from '../apiRequests';
+
+// const username = Cookies.get('savedLogin-username');
+// const role = Cookies.get('savedLogin-role');
+
+export async function redirectWhenLoginSaved() {
 	if (
 		(Cookies.get('savedLogin-username') != undefined ||
 			Cookies.get('savedLogin-email') != undefined) &&
 		Cookies.get('savedLogin-password') != undefined
-	) {
-		window.location.href = '/home';
-	}
+	){
+		await fetchDocuments('User').then((users) => {
+			for (let i of users) {
+				if (i.username === Cookies.get('savedLogin-username') && String(i.passwordHash) === Cookies.get('savedLogin-password') && i.role === Cookies.get('savedLogin-role')) {
+					window.location.href = '/home';
+				}
+			}
+		})
+	} 
+
 }
 
-export function redirectWhenNotLoggedIn() {
+export async function redirectWhenNotLoggedIn() {
 	if (
 		(Cookies.get('savedLogin-username') == undefined ||
 			Cookies.get('savedLogin-email') == undefined) &&
 		Cookies.get('savedLogin-password') == undefined
 	) {
-		window.location.href = '/';
+		await fetchDocuments('User').then((users) => {
+			for (let i of users) {
+				if (i.username != Cookies.get('savedLogin-username') && String(i.passwordHash) != Cookies.get('savedLogin-password') && i.role != Cookies.get('savedLogin-role')) {
+					window.location.href = '/';
+				}
+			}
+		})
 	}
+}
+
+/**
+ * Gets cookie information and verifies whether it matches with database collection
+ *
+ * @param {*} username username stored in cookie
+ * @param {*} password password stored in cookie
+ * @param {*} role role stored in cookie
+ * @return {*} true if cookie information matches database. False otherwise
+*/
+async function validCookie(username: any, password: any, role: any) {
+	// console.log(username)
+	// console.log(password)
+	// console.log(role)
+	await fetchDocuments('User').then((users) => {
+		for (let i of users) {
+			// console.log(i)
+			if (i.username === username && i.passwordHash === password && i.role === role) {
+				return true;
+				// console.log(valid);
+			}
+		}
+	})
+	return false;
 }
