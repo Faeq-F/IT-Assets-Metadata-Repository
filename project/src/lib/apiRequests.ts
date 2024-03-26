7d3b9bfd68b5db8333321223a7638a56d38c7f89
Faeq <faeqfaisal@hotmail.co.uk>
2024-03-26 16:55:28 +0000

    Code review - completed lib

--- project/src/lib/apiRequests.ts
+++ project/src/lib/apiRequests.ts
@@ -1,7 +1,16 @@
-//remember to wrap these in onMount checks
 import Cookies from 'js-cookie';
 
-function getCurrentUser() {
+/**
+ * Creates an object representing the user that is currently logged in, through browser cookies
+ * @returns an object representing the user that is currently logged in
+ * @author Christian-Frederick Cubos
+ * @email zlac145@live.rhul.ac.uk
+ */
+function getCurrentUser(): {
+	username: string | undefined;
+	passwordHash: string | undefined;
+	role: string | undefined;
+} {
 	return {
 		username: Cookies.get('savedLogin-username'),
 		passwordHash: Cookies.get('savedLogin-password'),
@@ -9,23 +18,33 @@ function getCurrentUser() {
 	};
 }
 
+//remember to wrap these in onMount checks
+
 /**
- * Gets documents from a collection in the database & returns them in json form
+ * Gets documents from a collection in the database & returns them in JSON form
  * @param collectionName The collection to fetch documents from
  * @returns The documents in the collection
+ * @author Faeq Faisal
+ * @email faeqfaisal@hotmail.co.uk
+ * @email zlac318@live.rhul.ac.uk
  */
-export const fetchDocuments = (collectionName: string) => {
+export const fetchDocuments = (collectionName: string): Promise<any> => {
 	return fetch('http://localhost:5038/api/get/collection/' + collectionName).then((response) => {
 		return response.json();
 	});
 };
 
 /**
- * Gets a document from the database & returns it in json form
+ * Gets a document from the database & returns it in JSON form
  * @param id The id of the document to fetch
  * @returns The fetched document, {} if it doesn't exist
+ * @author Faeq Faisal
+ * @email faeqfaisal@hotmail.co.uk
+ * @email zlac318@live.rhul.ac.uk
+ * @author Hossam Babari
+ * @email zlac241@live.rhul.ac.uk
  */
-export const fetchDocumentByID = (id: string) => {
+export const fetchDocumentByID = (id: string): Promise<any> => {
 	return fetch('http://localhost:5038/api/get/document/' + id).then((response) => {
 		return response.json();
 	});
@@ -34,10 +53,17 @@ export const fetchDocumentByID = (id: string) => {
 /**
  * Inserts a document into a collection in the database
  * @param collectionName The collection to insert a document into
- * @param formData The FormData object that holds the document to insert in the 'newData' key
+ * @param formData The FormData object that holds the document (stringified JSON) to insert in the 'newData' key
  * @returns Acknowledgment from the database
+ * @author Faeq Faisal
+ * @email faeqfaisal@hotmail.co.uk
+ * @email zlac318@live.rhul.ac.uk
+ * @author Hossam Babari
+ * @email zlac241@live.rhul.ac.uk
+ * @author Christian-Frederick Cubos
+ * @email zlac145@live.rhul.ac.uk
  */
-export const insertDocument = (collectionName: string, formData: any) => {
+export const insertDocument = (collectionName: string, formData: any): Promise<any> => {
 	formData.set('userData', JSON.stringify(getCurrentUser()));
 	return fetch('http://localhost:5038/api/insert/collection/' + collectionName, {
 		method: 'POST',
@@ -54,11 +80,18 @@ export const insertDocument = (collectionName: string, formData: any) => {
 /**
  * Updates a document in a collection from the database
  * @param collectionName The collection that holds the document to update
- * @param id The id of the document to update
- * @param formData The FormData object that holds the document to update in the 'newData' key. The document must have it's _id passed.
+ * @param id The _id of the document to update
+ * @param formData The FormData object that holds the document (stringified JSON) to update in the 'newData' key.
  * @returns Acknowledgment from the database
+ * @author Faeq Faisal
+ * @email faeqfaisal@hotmail.co.uk
+ * @email zlac318@live.rhul.ac.uk
+ * @author Hossam Babari
+ * @email zlac241@live.rhul.ac.uk
+ * @author Christian-Frederick Cubos
+ * @email zlac145@live.rhul.ac.uk
  */
-export const updateDocument = (collectionName: string, id: string, formData: any) => {
+export const updateDocument = (collectionName: string, id: string, formData: any): Promise<any> => {
 	// adds user details into formData
 	formData.set('userData', JSON.stringify(getCurrentUser()));
 	return fetch(
@@ -75,13 +108,18 @@ export const updateDocument = (collectionName: string, id: string, formData: any
 /**
  * Deletes a document from a collection in the database
  * @param collectionName The collection to delete a document from
- * @param documentID The _id value of the document to delete
- * @returns Acknowledgment from the database, including json representing the original document that has now been removed
+ * @param documentID The _id of the document to delete
+ * @returns Acknowledgment from the database, including JSON representing the original document that has now been removed
+ * @author Faeq Faisal
+ * @email faeqfaisal@hotmail.co.uk
+ * @email zlac318@live.rhul.ac.uk
+ * @author Hossam Babari
+ * @email zlac241@live.rhul.ac.uk
+ * @author Christian-Frederick Cubos
+ * @email zlac145@live.rhul.ac.uk
  */
-export const deleteDocument = (collectionName: string, documentID: string) => {
-	// create FormData object
+export const deleteDocument = (collectionName: string, documentID: string): Promise<any> => {
 	const formData = new FormData();
-	// have the FormData object include user credentials
 	formData.set('userData', JSON.stringify(getCurrentUser()));
 	return fetch(
 		'http://localhost:5038/api/delete/collection/' + collectionName + '/document/' + documentID,
